6e979816b396d5b291ad8ce9578dd04bbeb11d16
Abrar <amahi2001@gmail.com>
2021-09-03 12:52:39 -0400

    creating media dir, adding jobs to settings.py

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+
+    #creating a image field, storing the image in the images folder
+    image = models.ImageField(upload_to='images/')
+
+    #creating a summary field
+    summary = models.CharField(max_length=200)
